632eb6a13c76f23007b9ca8d0b52c428a7185b45
Crystal Huang <chuan147@ucsc.edu>
2023-02-05 11:14:49 -0800

    nevermind got rid of pieplot

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ import os
 import os
 from AI import AI
 from PIL import Image
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def app():
@@ -24,18 +24,18 @@ def app():
     # Adding the HTML string to Streamlit
     # st.markdown(html_string, unsafe_allow_html=True)
 
-    def analyze_week():
-       with open("weekly.txt", "r") as f:
-           indexes = {'meat':0, 'vegetable':1, 'bread':2, 'fruit':3}
-           counts = [0 for _ in range(len(indexes))]
-           for line in f:
-               counts[indexes[line[:-1]]] += 1
-           plt.pie(counts, labels=list(indexes.keys()))
-           plt.savefig("pie_chart.png")
-    analyze_week()
-    st.subheader("Your week has consisted of the following: ")
-    st.image("pie_chart.png")
-    st.subheader("Upload a new food entry below! ")
+    # def analyze_week():
+    #    with open("weekly.txt", "r") as f:
+    #        indexes = {'meat':0, 'vegetable':1, 'bread':2, 'fruit':3}
+    #        counts = [0 for _ in range(len(indexes))]
+    #        for line in f:
+    #            counts[indexes[line[:-1]]] += 1
+    #        plt.pie(counts, labels=list(indexes.keys()))
+    #        plt.savefig("pie_chart.png")
+    # analyze_week()
+    # st.subheader("Your week has consisted of the following: ")
+    # st.image("pie_chart.png")
+    # st.subheader("Upload a new food entry below! ")
     uploaded_file = st.file_uploader("Upload an image of food:", type=["jpg", "jpeg", "png",])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
