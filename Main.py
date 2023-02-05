42b5bda409af430db1e248b91163a3192e1da695
Andrew Hu <anlhu@ucsc.edu>
2023-02-04 18:54:42 -0800

    more functions

--- Main.py
+++ Main.py
@@ -6,3 +6,15 @@ im = list(im.getdata())
 a = AI(im)
 category, diff = a.classify()
 print(category, diff*100)
+
+with open("weekly.txt", "a") as f:
+    f.write(category)
+    f.write("\n")
+
+def move_weekly():
+    with open("archive.txt", "a") as archive:
+        with open("weekly.txt", "r") as weekly:
+            for line in weekly:
+                archive.write(line)
+    with open("weekly.txt", "w"):
+        pass
